779bce05185a734ff736c10ceb14e61a7d27c4b6
tanrihao7 <1612970558@qq.com>
2020-09-24 11:01:18 +0800

    SSM

---
+++ src/main/java/com/Exception/CustomExcption.java
@@ -0,0 +1,28 @@
+package com.Exception;
+
+public class CustomExcption extends Exception {
+    public String message;
+    public Throwable throwable;
+
+    public CustomExcption(String message) {
+        super(message);
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Throwable getThrowable() {
+        return throwable;
+    }
+
+    public void setThrowable(Throwable throwable) {
+        this.throwable = throwable;
+    }
+}
---
+++ src/main/java/com/Exception/CutomExcptionResolver.java
@@ -0,0 +1,30 @@
+package com.Exception;
+
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CutomExcptionResolver implements HandlerExceptionResolver {
+    @Override
+    public ModelAndView resolveException(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) {
+        // 解析出异常类型
+        CustomExcption customExcption;
+        // 若该异常类型是系统自定义的异常，直接取出异常信息在错误页面展示即可。
+        if(e instanceof CustomExcption){
+            customExcption = (CustomExcption)e;
+        }else{
+            // 如果不是系统自定义异常，构造一个系统自定义的异常类型，信息为“未知错误”
+            customExcption = new CustomExcption("未知错误");
+        }
+        //错误信息
+        String message = customExcption.getMessage();
+        ModelAndView modelAndView = new ModelAndView();
+        //将错误信息传到页面
+        modelAndView.addObject("message",message);
+        //指向错误页面
+        modelAndView.setViewName("showerror");
+        return modelAndView;
+    }
+}
